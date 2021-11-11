fc0781d7375321181db74533d1fa6b2c5bd34145
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2021-11-11 17:55:31 +0300

    clean up

--- lib/jwt_modules/jwt_handler.rb
+++ lib/jwt_modules/jwt_handler.rb
@@ -1,28 +1,5 @@
 require "app_exceptions/custom_errors"
 
-class TokensCore
-  attr_accessor :model
-
-  def initialize model
-    self.model = model 
-  end
-
-  protected
-  def generate_payload fields
-    raise CustomErrors.new("TokensCore generate payload error.\
-            unintialized model.") if model.nil?
-
-    payload= {}
-    fields.each do |field|
-      raise CustomErrors.new "TokenCore generate payload \
-          error: undefined method #{field} for model #{model}." unless model.methods.include?(field)
-          
-        payload[field] = model.send("#{field}")
-    end
-    payload
-  end
-end
-
 module JWT_Handler
 
   JWT_SECRET_KEY =  ENV['JWT_SECRET_KEY'] 
