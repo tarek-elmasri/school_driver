e1855bc4cc8fa97b40798b503e1864311ce139b3
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2021-12-20 08:44:38 +0300

    fixing syntax error

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -4,6 +4,6 @@ class Parent < ApplicationRecord
   has_many :drive_requests
 
   validates :first_name, presence: { message: I18n.t(:first_name_required)}
-  validates :last_name , presence: { message: I18n.t.(:last_name_required)}
+  validates :last_name , presence: { message: I18n.t(:last_name_required)}
 
 end
