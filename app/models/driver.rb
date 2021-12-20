c8ced3cd0233c5df624b9dc010a77c764c7d7182
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2021-12-20 10:51:20 +0300

    basic validations

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,4 +5,22 @@ class Driver < ApplicationRecord
   has_one_attached :id_card
   has_one_attached :car_license_card
 
+  validates :first_name, presence: {message: I18n.t(:first_name_required)}
+  validates :last_name, presence: { message: I18n.t(:last_name_required)}
+  validates :dob , presence: {message: I18n.t(:dob_required)}
+  validate :age_in_accepted_range
+
+
+
+  def age 
+    return unless dob
+    ((Time.now.utc - dob.to_time) / 1.year.seconds).floor
+  end
+
+
+  private
+  def age_in_accepted_range
+    return unless dob
+    errors.add(:age , I18n.t(:invalid_dob)) if age < 18 || age > 70
+  end
 end
